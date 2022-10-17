cdeb273957c104a62c959039c8f3f4a9db9cbe83
Elue Michael <meekyberry6@gmail.com>
2022-10-17 04:12:02 +0100

    Fizz Buzz

--- index.js
+++ index.js
@@ -46,3 +46,30 @@ const isUgly = (n) => {
  return n === 1;
 };
 console.log(isUgly(6));
+
+/*----------                               Fizz Buzz                             ----------*/
+/*=============================================
+// Given an integer n, return a string array answer (1-indexed) where:
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i if non of the above conditions are true.
+// Input: n = 3
+// Output: ["1", "2", "Fizz"]
+ =============================================*/
+
+const fizzBuzz = (n) => {
+ let answer = [];
+ for (let i = 1; i <= n; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+   answer.push("FizzBuzz");
+  } else if (i % 3 === 0) {
+   answer.push("Fizz");
+  } else if (i % 5 === 0) {
+   answer.push("Buzz");
+  } else {
+   answer.push(i.toString());
+  }
+ }
+ return answer;
+}
